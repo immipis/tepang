c190da358ba18a13524947c9a4e690442501f878
sin young261 <waterfired@naver.com>
2024-12-20 16:02:31 +0900

    branch이동.

--- TePang/src/main/java/com/tepang/TepangFrontControl.java
+++ TePang/src/main/java/com/tepang/TepangFrontControl.java
@@ -11,20 +11,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.tepang.common.Control;
+import com.tepang.control.ProductDetailControl;
 
-public class TepangFrontControl extends HttpServlet{
+public class TepangFrontControl extends HttpServlet {
 	Map<String, Control> map;
-	
+
 	public TepangFrontControl() {
 		map = new HashMap<>(); // 필드의 값을 초기화.
 	}
-	
+
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-		
-		
+		map.put("/product-detail.do", new ProductDetailControl());
+
 	}
-	
+
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// http://localhost:80/BoardWeb/boardList.do
@@ -33,7 +34,7 @@ public class TepangFrontControl extends HttpServlet{
 		String context = req.getContextPath();
 		String path = uri.substring(context.length());
 		System.out.println(path); // "/boardList.do" 공통부분을 제외한 나머지 부분.
-		
+
 		// 요청url === 실행할 컨트롤
 		Control control = map.get(path);
 		control.exec(req, resp);
---
+++ TePang/src/main/java/com/tepang/jdbc/ProductDAO.java
@@ -0,0 +1,9 @@
+package com.tepang.jdbc;
+
+import com.tepang.common.DAO;
+
+public class ProductDAO extends DAO {
+	
+	// 상품 상세.
+	Public 
+}
