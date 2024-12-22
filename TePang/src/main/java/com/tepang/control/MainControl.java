26a1c866ca063ccd304ca971ab0cd37d36180c9e
pc <pc@email.com>
2024-12-22 20:20:24 +0900

    tq

--- TePang/src/main/java/com/tepang/control/MainControl.java
+++ TePang/src/main/java/com/tepang/control/MainControl.java
@@ -1,23 +1,38 @@
 package com.tepang.control;
 
+import java.io.Console;
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.tepang.common.Control;
 import com.tepang.jdbc.MainDAO;
+import com.tepang.vo.MainVO;
 
 public class MainControl implements Control {
 
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		resp.setContentType("text/json;charset=utf-8");
 		MainDAO mdao = new MainDAO();
-		req.setAttribute("products", mdao.addpList("user01"));
+		
+		String logId = null;//req.getParameter("logId");
+		List<MainVO> products = mdao.addpList(logId);
+		   
+		req.setAttribute("products", products);
 		req.getRequestDispatcher("WEB-INF/html/main.jsp").forward(req, resp);
-
+		
+		
+//		Gson gson = new GsonBuilder().create();
+//		String json = gson.toJson(products);
+//		
+//		resp.getWriter().write(json);
 	}
 
 }
