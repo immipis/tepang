1d7bbb9d5ce5917e52854302fe3752d6c9e4d1ce
sin young261 <waterfired@naver.com>
2024-12-23 09:49:31 +0900

    Merge branch 'lsy' of https://github.com/immipis/tepang.git into lsy

--- TePang/src/main/java/com/tepang/control/ProductDetailControl.java
+++ TePang/src/main/java/com/tepang/control/ProductDetailControl.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.tepang.common.Control;
+import com.tepang.vo.BoardVO;
 
 public class ProductDetailControl implements Control {
 
