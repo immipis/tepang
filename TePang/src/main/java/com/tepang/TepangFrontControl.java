0861fae19461111eb3d04319a1e77d10626a2148
admin <admin@DESKTOP-RDAC6JF>
2024-12-31 21:56:27 +0900

    1231

--- TePang/src/main/java/com/tepang/TepangFrontControl.java
+++ TePang/src/main/java/com/tepang/TepangFrontControl.java
@@ -16,18 +16,18 @@ import com.tepang.control.BoardControl;
 import com.tepang.control.BoardFormControl;
 import com.tepang.control.BoardListControl;
 import com.tepang.control.CartListControl;
-import com.tepang.control.MainCartControl;
 import com.tepang.control.GetReviewCountControl;
 import com.tepang.control.InsertCartControl;
+import com.tepang.control.MainCartControl;
 import com.tepang.control.MainControl;
 import com.tepang.control.MyHeartListControl;
-import com.tepang.control.MyInfoUpdateControl;
-import com.tepang.control.MyInfoUpdatePageControl;
 import com.tepang.control.MyInfoControl;
 import com.tepang.control.MyInfoDeletePageControl;
 import com.tepang.control.MyInfoDropControl;
+import com.tepang.control.MyInfoUpdateControl;
+import com.tepang.control.MyInfoUpdatePageControl;
 import com.tepang.control.MyPageBoardControl;
-import com.tepang.control.MyReviewControl;
+import com.tepang.control.MyReplyControl;
 import com.tepang.control.PaymentControl;
 import com.tepang.control.ProductDetailControl;
 import com.tepang.control.ProductListControl;
@@ -87,14 +87,14 @@ public class TepangFrontControl extends HttpServlet {
 		
 		// 마이페이지
 		map.put("/myPage.do", new MyPageBoardControl()); // 마이페이지 상세
-		map.put("/myinfo.do", new MyInfoControl()); 	// 정보확인
+		map.put("/myInfo.do", new MyInfoControl()); 	// 정보확인
 
-		map.put("/myinfoupdatepage.do", new MyInfoUpdatePageControl()); // 정보 수정 페이지 
-		map.put("/myinfoUpdate.do", new MyInfoUpdateControl()); // 정보 수정 기능
-		map.put("/myinfoDeletePage.do", new MyInfoDeletePageControl()); // 회원탈퇴 페이지
+		map.put("/myInfoUpdatePage.do", new MyInfoUpdatePageControl()); // 정보 수정 페이지 
+		map.put("/myInfoUpdate.do", new MyInfoUpdateControl()); // 정보 수정 기능
+		map.put("/myInfoDeletePage.do", new MyInfoDeletePageControl()); // 회원탈퇴 페이지
 		map.put("/infoDelete.do", new MyInfoDropControl()); // 회원탈퇴 기능
-		map.put("/myreview.do", new MyReviewControl()); // 나의 문의 불러오기
-		map.put("/myheartlist.do", new MyHeartListControl()); // 나의 찜목록 불러오기
+		map.put("/myReply.do", new MyReplyControl()); // 나의 문의 불러오기
+		map.put("/myHeartList.do", new MyHeartListControl()); // 나의 찜목록 불러오기
 
         // 리뷰 이신영
 		map.put("/review.do", new ReviewControl()); // 목록
