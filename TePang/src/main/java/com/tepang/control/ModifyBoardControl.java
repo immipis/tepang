4247f9282434ae0fb87f47865b1e2681bad4488d
pc <pc@email.com>
2025-01-02 11:44:26 +0900

    20250102박지희3

---
+++ TePang/src/main/java/com/tepang/control/ModifyBoardControl.java
@@ -0,0 +1,19 @@
+package com.tepang.control;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.tepang.common.Control;
+
+public class ModifyBoardControl implements Control {
+
+	@Override
+	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+
+	}
+
+}
